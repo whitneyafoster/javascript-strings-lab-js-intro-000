0ea0167747590109d32160f9606e4a63dd24503a
Whitney <foster.whitney.a@gmail.com>
2019-02-02 22:27:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var specialGuest = "Hello, Neil deGrasse Tyson"
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
-var topic = "Let's talk about Let's talk about space.";
+var topic = "Let's talk about space.";
 
-var conversation = `${topic}`;
+var conversation = `${space}`;
